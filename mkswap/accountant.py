38b0c1b9325a94c4d930023f700708cd6be11898
bubbleboy14 <mario.balibrera@gmail.com>
2023-12-09 23:15:45 -0800

    adjusted Accountant for more general (non-dydx, and especially gemini) usage

--- mkswap/accountant.py
+++ mkswap/accountant.py
@@ -1,15 +1,9 @@
 from datetime import datetime
-from .backend import rel, ask, listen
+from .backend import rel, ask, listen, predefs
 from .base import Feeder
 
-defbals = {
-	"USD": 100,
-	"ETH": 0.05,
-	"BTC": 0.0025
-}
-
 class Accountant(Feeder):
-	def __init__(self, balances=defbals):
+	def __init__(self, platform=predefs["platform"], balances=predefs["balances"]):
 		self.counts = {
 			"filled": 0,
 			"approved": 0
@@ -19,8 +13,17 @@ class Accountant(Feeder):
 		self._balances = balances
 		self._obals.update(balances)
 		self.starttime = datetime.now()
+		self.platform = platform
+		self._usd = "USD"
+		if platform == "dydx":
+			rel.timeout(10, self.checkFilled)
+			self._usd = "-USD"
 		listen("affordable", self.affordable)
-		rel.timeout(10, self.checkFilled)
+
+	def pair(self, syms):
+		if self.platform == "dydx":
+			return syms.split("-")
+		return syms[:3], syms[3:]
 
 	def checkFilled(self):
 		self.counts["filled"] = 0
@@ -37,7 +40,7 @@ class Accountant(Feeder):
 			amount = bz[sym] - obz[sym]
 			v = vz[sym] = bz[sym]
 			if amount and sym != "USD":
-				price = pricer(sym + "-USD")
+				price = pricer(sym + self._usd)
 				amount *= price
 				vz[sym] = "%s ($%s)"%(v, v * price)
 			total += amount
@@ -49,22 +52,22 @@ class Accountant(Feeder):
 		s = prop.get("size", 10)
 		v = s / prop["price"]
 		bz = self._balances
-		sym = prop["symbol"].split("-")[0]
+		sym1, sym2 = self.pair(prop["symbol"])
 		if prop["symbol"] not in self.syms:
 			self.syms.append(prop["symbol"])
 		self.log("balances", bz)
 		if prop["action"] == "BUY":
-			if s > bz["USD"]:
-				self.log("not enough USD!")
+			if s > bz[sym2]:
+				self.log("not enough %s!"%(sym2,))
 				return False
-			bz["USD"] -= s
-			bz[sym] += v
+			bz[sym2] -= s
+			bz[sym1] += v
 		else:
-			if v > bz[sym]:
-				self.log("not enough %s!"%(sym,))
+			if v > bz[sym1]:
+				self.log("not enough %s!"%(sym1,))
 				return False
-			bz["USD"] += s
-			bz[sym] -= v
+			bz[sym2] += s
+			bz[sym1] -= v
 		self.counts["approved"] += 1
 		self.log("trade approved!")
 		return True
