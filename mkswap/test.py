3b98eacb6942649777af74b7d402f34376d62b1d
bubbleboy14 <mario.balibrera@gmail.com>
2023-12-10 02:34:06 -0800

    test: gemini stuff

--- mkswap/test.py
+++ mkswap/test.py
@@ -1,21 +1,36 @@
-from dydx3 import constants
 from .accountant import Accountant
-from .agent import Agent
-from .backend import start, spew
+from .agent import agencies
+from .backend import start, spew, predefs, GEMDOM
 
+plat = predefs["platform"]
+Agent = agencies[plat]
+SILENT_REQUEST = False
+
+#
+# mkswap tests
+#
 def get(path, ag=None):
-	from dez.http import fetch
+	from dez.http import fetch, post
+	getter = post
 	ag = ag or Agent()
 	hz = ag.credHead(path)
-	host = constants.API_HOST_GOERLI.split("//")[1]
+	if plat == "dydx":
+		getter = fetch
+		from dydx3 import constants
+		host = constants.API_HOST_GOERLI.split("//")[1]
+	else: # gemini
+		host = GEMDOM
 	print("fetching:", host, path)
-	fetch(host, path, port=443, secure=True, headers=hz, cb=spew, dispatch=True)
+	getter(host, path, port=443, secure=True, headers=hz, cb=spew, dispatch=True, silent=SILENT_REQUEST)
 
 def accountant(): # doesn't work - "Invalid id"
 	acc = Accountant()
 	ag = Agent()
 	start()
 
+#
+# dydx tests
+#
 def accountById(): # works!
 	ag = Agent()
 	get("/v3/accounts/" + ag.id() + "?ethereumAddress=" + ag.client.default_address, ag)
@@ -26,8 +41,15 @@ def accounts(): # works!
 def reg(): # works!
 	get("/v3/registration")
 
+#
+# gemini tests
+#
+def notionalVolume():
+	get("/v1/notionalvolume")
+
 if __name__ == "__main__":
-	accountant()
+	notionalVolume()
+	#accountant()
 	#accountById()
 	#accounts()
 	#reg()
