cd02dca6d3b7f9a6bb0c98c11b23bcf0a078fba5
bubbleboy14 <mario.balibrera@gmail.com>
2023-12-10 23:06:25 -0800

    Comptroller proc(), on_message(), etc

--- mkswap/comptroller.py
+++ mkswap/comptroller.py
@@ -1,19 +1,33 @@
 from .backend import listen, gemget
 from .base import Feeder
 
+orderNumber = 0
+
 class Comptroller(Feeder):
 	def __init__(self):
-		self.actives = []
+		self.actives = {}
 		self.backlog = []
 		listen("priceChange", self.curate)
 		listen("enqueueOrder", self.enqueue)
 		self.feed("gemorders")
 
-	def on_message(self, ws, msg):
-		# TODO
-		# - update actives[] ; refill()
-		self.log("message:")
-		pprint(msg)
+	def proc(self, msg):
+		coi = msg.get("client_order_id", None)
+		if not coi:
+			return self.log("proc(%s): NO client_order_id!!!"%(msg,))
+		order = self.actives[coi]
+		etype = msg["type"]
+		if etype == "closed":
+			self.log("proc(): trade closed", order)
+			del self.actives[coi]
+		else:
+			self.log("proc(): %s"%(etype,))
+
+	def on_message(self, ws, msgs):
+		self.log("message:", msgs)
+		for msg in msgs:
+			self.proc(msg)
+		self.refill()
 
 	def curate(self):
 		# TODO
@@ -22,11 +36,13 @@ class Comptroller(Feeder):
 		pass
 
 	def refill(self):
-		while self.backlog and len(self.actives) < 10:
+		while self.backlog and len(self.actives.keys()) < 10:
 			self.submit(self.backlog.pop(0))
 
 	def submit(self, trade):
-		self.actives.append(trade)
+		orderNumber += 1
+		self.actives[orderNumber] = trade
+		trade["client_order_id"] = orderNumber
 		gemget("/v1/order/new", self.log, trade)
 
 	def enqueue(self, trade):
