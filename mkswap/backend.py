fe798d7b2819024ce31c97518546c7fa3422db32
bubbleboy14 <mario.balibrera@gmail.com>
2023-12-10 17:14:49 -0800

    backend: extracted a bunch of functions that now live in rel.util

--- mkswap/backend.py
+++ mkswap/backend.py
@@ -1,61 +1,6 @@
-import os, sys, rel, json, base64, websocket
-
-def log(*msg):
-	print(*msg)
-
-def read(fname):
-	if not os.path.exists(fname):
-		return
-	f = open(fname, 'r')
-	data = f.read()
-	f.close()
-	return data and json.loads(base64.b64decode(data).decode())
-
-def write(fname, data):
-	f = open(fname, 'w')
-	f.write(base64.b64encode(json.dumps(data).encode()).decode())
-	f.close()
-
-membank = {}
-remembered = read(".membank")
-remembered and membank.update(remembered)
-
-def remember(key, data, ask=True):
-	if ask and input("remember %s for next time? [Y/n] "%(key,)).lower().startswith("n"):
-		return log("ok, not remembering", key)
-	membank[key] = data
-	write(".membank", membank)
-
-def recall(key):
-	return membank.get(key, None)
-
-def memget(key, default=None):
-	val = recall(key)
-	if not val:
-		pstr = "%s? "%(key,)
-		if default:
-			pstr = "%s[default: %s] "%(pstr, default)
-		val = input(pstr) or default
-		remember(key, val)
-	return val
-
-listeners = {}
-def emit(channel, *data): # all cbs called, no return value
-	if channel not in listeners:
-		return print("%s: no one's listening"%(channel,))
-	for cb in listeners[channel]:
-		cb(*data)
-
-def ask(channel, *data): # only 1st cb called, data returned
-	if channel not in listeners:
-		return print("%s: no one's listening"%(channel,))
-	for cb in listeners[channel]:
-		return cb(*data)
-
-def listen(channel, cb):
-	if channel not in listeners:
-		listeners[channel] = []
-	listeners[channel].append(cb)
+import rel, json, websocket
+from rel import start, stop
+from rel.util import log, read, write, remember, recall, memget, emit, ask, listen
 
 def gemget(path, cb):
 	from dez.http import post
@@ -247,14 +192,3 @@ def spew(event):
 	if hasattr(event, "decode"):
 		event = event.decode()
 	log(json.dumps(event))
-
-def start():
-	rel.signal(2, rel.abort)
-	rel.dispatch()
-
-def stop():
-	log("goodbye")
-	if rel.is_running():
-		rel.abort()
-	else:
-		sys.exit()
