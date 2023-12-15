5c194e30afc2b317c48fdc4f1dc84dcbe74cc7a5
bubbleboy14 <mario.balibrera@gmail.com>
2023-12-14 19:08:34 -0800

    Comptroller.submit() uses gemtrade()

--- mkswap/comptroller.py
+++ mkswap/comptroller.py
@@ -1,5 +1,5 @@
 import json
-from .backend import listen, emit, gemget
+from .backend import listen, emit, gemget, gemtrade
 from .base import Feeder
 
 LIVE = False
@@ -103,7 +103,7 @@ class Comptroller(Feeder):
 		orderNumber += 1
 		self.actives[str(orderNumber)] = trade
 		trade["client_order_id"] = str(orderNumber)
-		LIVE and gemget("/v1/order/new", self.submitted, trade)
+		LIVE and gemtrade(trade, self.submitted)
 		emit("orderActive", trade)
 
 	def submitted(self, resp):
