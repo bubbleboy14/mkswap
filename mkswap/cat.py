dc75d0f8a78a13794d8bd5bd090d38172033dfe0
bubbleboy14 <mario.balibrera@gmail.com>
2023-12-14 19:10:17 -0800

    Cat: tick(), churn(), relay()

--- mkswap/cat.py
+++ mkswap/cat.py
@@ -1,7 +1,8 @@
 # Copy And Trade
+import rel
 from .base import Worker
 from .observer import Observer
-from .backend import start, setStaging, predefs, gemget
+from .backend import start, setStaging, predefs, gemtrade
 
 class Cat(Worker):
 	def __init__(self, symbols=["ETHUSD", "BTCUSD"], platform=predefs["platform"]):
@@ -9,16 +10,34 @@ class Cat(Worker):
 		self.symbols = symbols
 		self.platform = platform
 		self.watchers = {}
+		self.pending = []
+		self.relayed = 0
 		for symbol in symbols:
 			self.setWatcher(symbol)
 		setStaging(True)
+		rel.timeout(0.1, self.churn)
+		rel.timeout(1, self.tick)
+
+	def tick(self):
+		self.log("relayed:", self.relayed, "; pending:", len(self.pending))
+
+	def churn(self):
+		if self.pending:
+			self.relayed += 1
+			gemtrade(self.pending.pop(0))
 
 	def setWatcher(self, symbol):
 		self.watchers[symbol] = Observer(self.platform,
 			symbol, lambda event : self.relay(symbol, event))
 
 	def relay(self, symbol, event):
-		self.log("relay", symbol, event)
+#		self.log("relay", symbol, event)
+		self.pending.append({
+			"symbol": symbol,
+			"price": event["price"],
+			"amount": event["delta"],
+			"side": (event["side"] == "ask") and "buy" or "sell"
+		})
 
 if __name__ == "__main__":
 	Cat()
