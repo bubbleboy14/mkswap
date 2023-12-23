208c3dfeea2a16060061505606161e6ac634c63c
bubbleboy14 <mario.balibrera@gmail.com>
2023-12-22 17:10:37 -0800

    gem.Req: better logging

--- mkswap/gem.py
+++ mkswap/gem.py
@@ -2,6 +2,8 @@ import rel, random
 from .backend import ask, spew, die, dpost
 from .base import Worker
 
+oidkeys = ["client_order_id", "order_id"]
+
 class Req(Worker):
 	def __init__(self, path, gem, params={}, cb=spew):
 		self.cb = cb
@@ -9,20 +11,27 @@ class Req(Worker):
 		self.path = path
 		self.attempt = 0
 		self.params = params
-		self.log(path, params)
+		self.name = path
+		for key in oidkeys:
+			if key in params:
+				self.name = "%s %s"%(self.name, params[key])
+		self.log(params)
+
+	def sig(self):
+		return "Req[%s]"%(self.name,)
 
 	def get(self):
 		self.attempt += 1
-		self.log("get(%s, %s)"%(self.path, self.attempt))
+		self.log("get(%s)"%(self.attempt,))
 		dpost(self.path, ask("credHead", self.path, self.params), self.receive, self.retry)
 
 	def resubmit(self):
-		self.log("resubmit(%s) passing self back to gem - attempt %s"%(self.path, self.attempt))
+		self.log("resubmit(%s)"%(self.attempt,))
 		self.gem.add(self)
 
 	def retry(self, reason, timeout=None):
-		timeout = timeout or random.randint(5, 20)
-		self.log("retry(%s #%s)[%s] %s seconds!"%(self.path, self.attempt, reason, timeout))
+		timeout = timeout or random.randint(2, 10)
+		self.log("retry(%s)[%s] %s seconds!"%(self.attempt, reason, timeout))
 		rel.timeout(timeout, self.resubmit)
 
 	def decommission(self):
@@ -34,7 +43,7 @@ class Req(Worker):
 			return (self.cb or self.log)(res)
 		reason = res["reason"]
 		message = res["message"]
-		self.log("receive(%s) %s error: %s"%(self.path, reason, message))
+		self.log("receive(%s) %s error: %s"%(self.attempt, reason, message))
 		if reason not in ["RateLimit", "RateLimited", "InvalidNonce"]:
 			return die(reason, res)
 		if reason == "RateLimit":
