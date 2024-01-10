38e93cd8e3be2eff2be0049ceb285db2c78cba32
bubbleboy14 <mario.balibrera@gmail.com>
2024-01-10 01:58:46 -0800

    turb->turbulence

--- mkswap/strategy/slosh.py
+++ mkswap/strategy/slosh.py
@@ -105,7 +105,7 @@ class Slosh(Base):
 		volatility = self.volatility(cur, sigma)
 		emit("quote", "sigma", sigma)
 		emit("quote", "volatility", volatility)
-		emit("quote", "turb", sigma * sqrt(min(OUTER, len(self.allratios))))
+		emit("quote", "turbulence", sigma * sqrt(min(OUTER, len(self.allratios))))
 		print("\n\nsigma", sigma,
 			"\nvolatility", volatility,
 			"\ncurrent", cur,
