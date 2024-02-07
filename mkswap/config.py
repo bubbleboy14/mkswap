77adf7d01f768a036b2a2922921b7cad313b793f
bubbleboy14 <mario.balibrera@gmail.com>
2024-02-07 10:37:55 -0800

    config: replaced custom Config class w/ fyg Config instance

--- mkswap/config.py
+++ mkswap/config.py
@@ -1,164 +1,53 @@
-class Config(object):
-	def select(self):
-		from cantools.util.io import selnum
-		from .backend import predefs, presets
-		print("\nnoting Office defaults (%s), please select a configuration from the following presets.\n"%(predefs,))
-		return selnum(presets)
+from fyg import Config
 
-	def get(self, *path):
-		cur = self.current()
-		for k in path:
-			cur = cur[k]
-		return cur
-
-	def current(self):
-		from .comptroller import ACTIVES_ALLOWED, PRUNE_LIMIT, LIVE
-		from .office import VERBOSE, STAGISH
-		from .accountant import CAPPED, NUDGE
-		from .base import UNSPAMMED
-		from .ndx import INNER, SHORT, LONG, OUTER
-		from .strategy import base, rsi, slosh
-		from .backend import STAGING, REALDIE, CREDSET
-		from .harvester import SKIM, BATCH, BOTTOM, BALANCE, NETWORK
-		return {
-			"harvester": {
-				"skim": SKIM,
-				"batch": BATCH,
-				"bottom": BOTTOM,
-				"balance": BALANCE,
-				"network": NETWORK
-			},
-			"accountant": {
-				"nudge": NUDGE,
-				"capped": CAPPED
-			},
-			"comptroller": {
-				"actives": ACTIVES_ALLOWED,
-				"prunelimit": PRUNE_LIMIT,
-				"live": LIVE
-			},
-			"backend": {
-				"staging": STAGING,
-				"realdie": REALDIE,
-				"credset": CREDSET
-			},
-			"office": {
-				"verbose": VERBOSE,
-				"stagish": STAGISH
-			},
-			"base": {
-				"unspammed": UNSPAMMED
-			},
-			"ndx": {
-				"inner": INNER,
-				"short": SHORT,
-				"long": LONG,
-				"outer": OUTER
-			},
-			"strategy": {
-				"base": {
-					"loud": base.LOUD
-				},
-				"rsi": {
-					"size": rsi.TRADE_SIZE,
-					"period": rsi.RSI_PERIOD
-				},
-				"slosh": {
-					"oneswap": slosh.ONESWAP,
-					"randlim": slosh.RANDLIM,
-					"vmult": slosh.VOLATILITY_MULT,
-					"vcutoff": slosh.VOLATILITY_CUTOFF
-				}
-			}
+config = Config({
+	"accountant": {
+		"nudge": "auto",
+		"capped": "auto"
+	},
+	"backend": {
+		"staging": True,
+		"realdie": True,
+		"credset": "default"
+	},
+	"base": {
+		"unspammed": True
+	},
+	"comptroller": {
+		"live": False,
+		"actives": 20,
+		"prunelimit": 0.1
+	},
+	"harvester": {
+		"batch": 10,
+		"bottom": 40,
+		"skim": False,
+		"balance": True,
+		"network": "bitcoin"
+	},
+	"ndx": {
+		"inner": 16,
+		"short": 32,
+		"long": 64,
+		"outer": 128
+	},
+	"office": {
+		"verbose": False,
+		"stagish": False
+	},
+	"strategy": {
+		"base": {
+			"loud": False
+		},
+		"rsi": {
+			"size": 10,
+			"period": 14
+		},
+		"slosh": {
+			"vmult": 16,
+			"vcutoff": 0.8
+			"randlim": 0.002,
+			"oneswap": "auto"
 		}
-
-	def _set(self, c, s):
-		if type(c) is dict:
-			for k in c.keys():
-				self._set(c[k], s[k])
-		else:
-			s(c)
-
-	def set(self, c):
-		from .comptroller import setPruneLimit, setActives, setLive
-		from .office import setVerbose, setStagish
-		from .backend import setStaging, setRealDie, setCredSet
-		from .accountant import setCapped, setNudge
-		from .base import setUnspammed
-		from .strategy import base, rsi, slosh
-		from .ndx import setInner, setShort, setLong, setOuter
-		from .harvester import setSkim, setBatch, setBottom, setBalance, setNetwork
-		s = { # live/staging/stagish/credset should only be flipped by init()
-			"harvester": {
-				"skim": setSkim,
-				"batch": setBatch,
-				"bottom": setBottom,
-				"balance": setBalance,
-				"network": setNetwork
-			},
-			"accountant": {
-				"nudge": setNudge,
-				"capped": setCapped
-			},
-			"comptroller": {
-				"live": setLive,
-				"actives": setActives,
-				"prunelimit": setPruneLimit
-			},
-			"office": {
-				"verbose": setVerbose,
-				"stagish": setStagish
-			},
-			"backend": {
-				"staging": setStaging,
-				"realdie": setRealDie,
-				"credset": setCredSet
-			},
-			"base": {
-				"unspammed": setUnspammed
-			},
-			"ndx": {
-				"inner": setInner,
-				"short": setShort,
-				"long": setLong,
-				"outer": setOuter
-			},
-			"strategy": {
-				"base": {
-					"loud": base.setLoud
-				},
-				"rsi": {
-					"size": rsi.setSize,
-					"period": rsi.setPeriod
-				},
-				"slosh": {
-					"oneswap": slosh.setOneSwap,
-					"randlim": slosh.setRandLim,
-					"vmult": slosh.setVolatilityMult,
-					"vcutoff": slosh.setVolatilityCutoff
-				}
-			}
-		}
-		print("CONFIG SET", c)
-		self._set(c, s)
-
-	def init(self):
-		from cantools.util.io import selnum
-		cfg = {}
-		if input("\nlive comptroller? [No/yes] ").lower().startswith("y"):
-			cfg["comptroller"] = {
-				"live": True
-			}
-		print("\nselect mode")
-		mode = selnum(["staging", "stagish", "production"])
-		if mode == "production":
-			cfg["backend"] = {
-				"staging": False
-			}
-		elif mode == "stagish":
-			cfg["office"] = {
-				"stagish": True
-			}
-		cfg and self.set(cfg)
-
-config = Config()
+	}
+})
