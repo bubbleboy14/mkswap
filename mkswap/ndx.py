9140172605b89cc896700fa6aee1c36984303e35
bubbleboy14 <mario.balibrera@gmail.com>
2024-03-05 00:32:30 -0800

    NDX histories{trade,ask,bid} overhaul

--- mkswap/ndx.py
+++ mkswap/ndx.py
@@ -20,9 +20,9 @@ class NDX(Worker):
 		self.ratios = {}
 		self.orders = {}
 		self._volumes = {}
-		self.histories = {}
 		self.observers = {}
 		self.orderBook = {}
+		self.histories = { "trade": {}, "ask": {}, "bid": {} }
 		listen("mad", self.mad)
 		listen("fave", self.fave)
 		listen("price", self.price)
@@ -38,12 +38,15 @@ class NDX(Worker):
 		listen("observersReady", self.observersReady)
 		listen("updateOrderBook", self.updateOrderBook)
 
-	def updateOrderBook(self, symbol, side, price, remaining):
+	def updateOrderBook(self, symbol, event):
 		if symbol not in self.orders:
 			self.orders[symbol] = {}
 			self.orderBook[symbol] = { "bid": {}, "ask": {} }
-		self.orderBook[symbol][side][price] = remaining
+		side = event["side"]
+		price = float(event["price"])
+		self.orderBook[symbol][side][price] = float(event["remaining"])
 		self.orders[symbol][side] = price
+		self.incVol(symbol, float(event["delta"]))
 
 	def volumes(self):
 		vols = self._volumes.copy()
@@ -51,26 +54,26 @@ class NDX(Worker):
 			self._volumes[v] = 0
 		return vols
 
-	def price(self, symbol, fullSym=False):
+	def price(self, symbol, fullSym=False, history="trade"):
 		if fullSym:
 			symbol = ask("fullSym", symbol)
-		if symbol in self.histories:
-			return self.histories[symbol]["current"]
+		if symbol in self.histories[history]:
+			return self.histories[history][symbol]["current"]
 
-	def bestPrice(self, sym, side, span="inner"):
-		return round(self.histories[sym][span][side2height[side]], 6)
+	def bestPrice(self, sym, side, span="inner", history="trade"):
+		return round(self.histories[history][sym][span][side2height[side]], 6)
 
-	def bestPrices(self, sym, side, spans=BESTIES):
+	def bestPrices(self, sym, side, spans=BESTIES, history="trade"):
 		d = {}
 		for span in spans:
-			d[span] = self.bestPrice(sym, side, span)
+			d[span] = self.bestPrice(sym, side, span, history)
 		return d
 
-	def markets(self, sym, side="buy"):
+	def markets(self, sym, side="buy", history="trade"):
 		marks = {}
 		buyer = []
 		seller = []
-		for fullSym in self.histories:
+		for fullSym in self.histories[history]:
 			if fullSym.startswith(sym):
 				buyer.append(fullSym)
 			elif fullSym.endswith(sym):
@@ -89,14 +92,14 @@ class NDX(Worker):
 		self.observers[sym] = Observer(sym,
 			observe=lambda e : self.observed(sym, float(e["price"]), float(e["amount"])))
 
-	def observed(self, sym, price, volume):
-		config.base.unspammed or self.log("observed(%s %s @ %s)"%(volume, sym, price))
-		self.quote(sym, price, volume=volume, fave=True)
+	def observed(self, sym, price, volume, history="trade"):
+		config.base.unspammed or self.log("observed(%s %s %s @ %s)"%(volume, sym, history, price))
+		self.quote(sym, price, volume=volume, fave=True, history=history)
 
-	def observersReady(self):
+	def observersReady(self, history="trade"):
 		for sym in self.observers:
-			if sym not in self.histories:
-				self.log("no history for", sym, "!!!!!")
+			if sym not in self.histories[history]:
+				self.log("no", history, "history for", sym, "!!!!!")
 				return False
 		return True
 
@@ -128,21 +131,22 @@ class NDX(Worker):
 			return 0
 		return (rstats["current"] - rstats[span]) / sigma
 
-	def ave(self, symbol, limit=None, ratio=False):
+	def ave(self, symbol, limit=None, ratio=False, history="trade"):
 		if ratio:
 			top, bot = symbol
 			rats = self.ratios[top][bot]["all"]
 		elif type(symbol) is list:
 			rats = symbol
 		else:
-			rats = self.histories[symbol]["all"]
+			rats = self.histories[history][symbol]["all"]
 		if limit:
 			rats = rats[-limit:]
 		return sum(rats) / len(rats)
 
-	def ratio(self, top, bot, update=False):
+	def ratio(self, top, bot, update=False, history="trade"):
 		lpref = "ratio(%s/%s)"%(top, bot)
-		if top not in self.histories or bot not in self.histories:
+		hists = self.histories[history]
+		if top not in hists or bot not in hists:
 			return self.log("%s not ready - check back later"%(lpref,))
 		if top not in self.ratios:
 			self.ratios[top] = {}
@@ -153,7 +157,7 @@ class NDX(Worker):
 		rstats = self.ratios[top][bot]
 		if update:
 			tb = [top, bot]
-			rat = self.price(top) / self.price(bot)
+			rat = self.price(top, history=history) / self.price(bot, history=history)
 			if "current" not in rstats:
 				rstats["current"] = rstats["high"] = rstats["low"] = rat
 			else:
@@ -163,32 +167,35 @@ class NDX(Worker):
 				elif rat < rstats["low"]:
 					rstats["low"] = rat
 			rstats["all"].append(rat)
-			rstats["total"] = self.ave(tb, ratio=True)
+			rstats["total"] = self.ave(tb, ratio=True, history=history)
 			for span in SPANS:
-				rstats[span] = self.ave(tb, getSpan(span), True)
+				rstats[span] = self.ave(tb, getSpan(span), True, history)
 		return rstats
 
 	def fave(self, key, val):
 		self.faves[key] = val
 
-	def quote(self, symbol, price, volume=None, fave=False):
-		if symbol not in self.histories:
-			self.histories[symbol] = {
+	def incVol(self, symbol, volume):
+		if symbol not in self._volumes:
+			self._volumes[symbol] = 0
+		self._volumes[symbol] += volume
+
+	def quote(self, symbol, price, volume=None, fave=False, history="trade"):
+		hists = self.histories[history]
+		if symbol not in hists:
+			hists[symbol] = {
 				"all": [],
 				"inner": {},
 				"short": {},
 				"long": {},
 				"outer": {}
 			}
-		if volume:
-			if symbol not in self._volumes:
-				self._volumes[symbol] = 0
-			self._volumes[symbol] += volume
+		volume and self.incVol(symbol, volume)
 		fave and self.fave(symbol, price)
-		symhis = self.histories[symbol]
+		symhis = hists[symbol]
 		symhis["current"] = price
 		symhis["all"].append(price)
-		symhis["average"] = self.ave(symbol)
+		symhis["average"] = self.ave(symbol, history=history)
 		for span in SPANS:
 			stretch = symhis["all"][-getSpan(span):]
 			symhis[span]["high"] = max(stretch)
