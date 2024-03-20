eeef7e7191b4e2ac0ea84bac5566e8929a2ecf2f
bubbleboy14 <mario.balibrera@gmail.com>
2024-03-20 12:25:59 -0700

    MultiFeed: sub(), extracted from subs(); on_open() re-sub()s subs

--- mkswap/multifeed.py
+++ mkswap/multifeed.py
@@ -15,6 +15,12 @@ class MultiFeed(Feeder):
 		self.start_feed()
 		listen("mfsub", self.subscribe)
 
+	def on_open(self):
+		self.log("opened")
+		for sym in self.subscriptions:
+			for mode in self.subscriptions[sym]:
+				self.sub(sym, mode)
+
 	def l2(self, change, sym):
 		return {
 			"symbol": sym,
@@ -51,18 +57,21 @@ class MultiFeed(Feeder):
 		for sub in self.subs(sym, mode):
 			sub(events)
 
+	def sub(self, symbol, mode):
+		self.ws.jsend({
+			"type": "subscribe",
+			"subscriptions": [{
+				"name": mode,
+				"symbols": [symbol]
+			}]
+		})
+
 	def subs(self, symbol, mode="l2"):
 		if symbol not in self.subscriptions:
 			self.subscriptions[symbol] = {}
 		if mode not in self.subscriptions[symbol]:
 			self.subscriptions[symbol][mode] = []
-			self.ws.jsend({
-				"type": "subscribe",
-				"subscriptions": [{
-					"name": mode,
-					"symbols": [symbol]
-				}]
-			})
+			self.sub(symbol, mode)
 		return self.subscriptions[symbol][mode]
 
 	def subscribe(self, symbol, cb, mode="l2"):
