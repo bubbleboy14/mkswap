82da57db64564a2d7444a5dd83ef4fbea88f2c8d
bubbleboy14 <mario.balibrera@gmail.com>
2024-04-12 18:48:57 -0700

    Booker bests{}

--- mkswap/booker.py
+++ mkswap/booker.py
@@ -3,19 +3,27 @@ from .base import Worker
 
 class Booker(Worker):
 	def __init__(self):
+		self.bests = {}
 		self.totes = {}
 		self.orders = {}
 		self.orderBook = {}
+		listen("bestOrder", self.bestOrder)
 		listen("updateOrderBook", self.updateOrderBook)
+
+	def bestOrder(self, symbol, side):
+		return self.bests[symbol][side]
 
 	def updateOrderBook(self, symbol, event):
 		if symbol not in self.orders:
+			self.bests[symbol] = {}
 			self.orders[symbol] = {}
 			self.orderBook[symbol] = { "bid": {}, "ask": {} }
 		side = event["side"]
+		isask = side == "ask"
 		price = float(event["price"])
 		self.orderBook[symbol][side][price] = float(event["remaining"])
 		self.orders[symbol][side] = price
+		self.bests[symbol][side] = (isask and min or max)(self.orderBook[symbol][side].keys())
 		emit("quote", symbol, price, volume=float(event["remaining"]), history=side)
 
 	def totals(self):
