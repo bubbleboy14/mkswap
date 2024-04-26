7689775b63e2b962783be41fa854e6ea6845666c
bubbleboy14 <mario.balibrera@gmail.com>
2024-04-25 18:59:56 -0700

    depv update: rel 0.4.9.13

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     zip_safe = False,
     install_requires = [
         "fyg >= 0.1.4",
-        "rel >= 0.4.9.8",
+        "rel >= 0.4.9.13",
         "websocket-client >= 1.7.0"
     ],
     entry_points = '''''',
