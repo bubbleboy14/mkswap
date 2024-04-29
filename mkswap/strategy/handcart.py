56d89b049b284ab028dc7826925b93160d240a27
bubbleboy14 <mario.balibrera@gmail.com>
2024-04-29 15:44:17 -0700

    handcart strategy

---
+++ mkswap/strategy/handcart.py
@@ -0,0 +1,42 @@
+from rel.util import ask, emit, listen
+from .base import Base
+from ..config import config
+
+hcfg = config.strategy.handcart
+
+class HandCart(Base):
+	def __init__(self, symbol, recommender=None):
+		Base.__init__(self, symbol, recommender)
+		emit("tellMeWhen", self.symbol, "price", -hcfg.threshold, self.start)
+		listen("orderFilled", self.filled)
+		self.log("loaded")
+
+	def filled(self, trade):
+		if trade["remaining"]:
+			return self.log("order filled ... incomplete")
+		side = trade["side"] == "buy" and "sell" or "buy"
+		self.order(side, self.nextPrice)
+
+	def orderAmount(self, side):
+		sym1, sym2 = self.symbol[:3], self.symbol[-3:]
+		bals = ask("balances", mode="actual")
+		bal = bals[side == "buy" and sym2 or sym1]
+		return bal * hcfg.risk
+
+	def order(self, side, price):
+		order = {
+			"side": side,
+			"price": price,
+			"symbol": self.symbol,
+			"amount": self.orderAmount(side)
+		}
+		pdiff = price * hcfg.profit
+		if side == "buy":
+			pdiff *= -1
+		self.nextPrice = price + pdiff
+		self.log("order(%s, %s) nextPrice: %s"%(side,
+			price, self.nextPrice), order)
+		emit("trade", order)
+
+	def start(self):
+		self.order("buy", ask("bestOrder", self.symbol, side))
